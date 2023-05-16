e9a9048a5d22ea3689c2dc0e98ef72d41233cd10
woollard <woollard@ebi.ac.uk>
2023-05-16 18:17:37 +0100

    initial

---
+++ source/analyse_mixs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Script of 'analyse_mixs.py' is to analyse the mixs
+
+___author___ = "woollard@ebi.ac.uk"
+___start_date___ = 2023-05-16
+__docformat___ = 'reStructuredText'
+
+"""
+
+
+from icecream import ic
+
+import argparse
+
+def get_data():
+    """"""
+
+    ic()
+
+def main():
+    get_data()
+
+if __name__ == '__main__':
+    ic()
+    main()
