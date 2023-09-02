64698e65292af927d9881058f79f2fa7f7ff78dd
woollard <woollard@ebi.ac.uk>
2023-09-02 06:53:52 +0100

    moved compareAllTerms and term_alignment_dict2df to pairwise_term_matches.py

--- source/analyse_mixs.py
+++ source/analyse_mixs.py
@@ -26,9 +26,9 @@ import plotly.express as px
 # from wordcloud import WordCloud
 
 import plotly.io as pio
-from rapidfuzz import process
+
 # project objects etc. being imported
-from pairwise_term_matches import pairwise_term_matches
+from pairwise_term_matches import pairwise_term_matches, compareAllTerms
 from COMPARISONS import COMPARISONS
 import mixs
 from mixs import mixs
@@ -575,18 +575,7 @@ def compare2termLists(left_term_list, right_term_list, comparisonStatsPackage):
     return comparisonStatsPackage
 
 
-def term_alignment_dict2df(my_dict):
-    """
 
-    :param my_dict:
-    :return: df
-    df = df[["left_term", "match_type", "match", "fuzzy_score"]]
-    """
-    df = pd.DataFrame.from_dict(my_dict, orient = 'index')
-    df["left_term"] = df.index
-    df = df[["left_term", "match_type", "match", "fuzzy_score"]]
-    df["fuzzy_score"] = df["fuzzy_score"].astype(int)
-    return df
 
 
 def compare2packages(comparison, left_package_name, right_package_name, left_obj, right_obj, comparisonStats, report):
@@ -798,63 +787,6 @@ def compareChecklists(ena_cl_obj, mixs_v6_obj, report):
 
     return comparison_obj
 
-
-def compareAllTerms(left_list, right_list):
-    """
-
-    :param left_list:
-    :param right_list:
-    :return: df: #['left_term', 'match_type:exact|harmonised|fuzzy|none', 'match', 'fuzzy_score', 'match_term_duplicated:boolean']
-    """
-    #make sure that the lists are unique and sorted, note the slight name change, in case we need the original list again
-    left_term_set = set(left_list)
-    right_term_set = set(right_list)
-    left_term_list = sorted(left_term_set)
-    right_term_list = sorted(right_term_set)
-    # ic(f"left len={len(left_term_list)} right len={len(right_term_list)}")
-
-    left_clean_dict, left_raw_dict = generate_clean_dict(left_term_list)
-    right_clean_dict, right_raw_dict = generate_clean_dict(right_term_list)
-
-    left_clean_set = set(left_clean_dict.keys())
-    right_clean_set = set(right_clean_dict.keys())
-    exact_intersection_set = left_term_set.intersection(right_term_set)
-    clean_intersection_set = left_clean_set.intersection(right_clean_set)
-
-    my_dict = {}
-    fuzzy_threshold = 85
-    for left_term in left_term_list:
-        my_dict[left_term] = {"match_type": "none", "fuzzy_score": 100}
-        left_clean = left_raw_dict[left_term]
-        # print(f"{left_term} - {left_clean}")
-        if left_term in exact_intersection_set:
-            my_dict[left_term]["match"] = left_term
-            my_dict[left_term]["match_type"] = "exact"
-        elif left_clean in clean_intersection_set:
-            my_dict[left_term]["match"] = right_clean_dict[left_clean]
-            my_dict[left_term]["match_type"] = "harmonised"
-        else:
-            resp_match = process.extractOne(left_term, right_term_list)
-            if resp_match[1] > fuzzy_threshold:
-                my_dict[left_term]["match_type"] = "fuzzy"
-                my_dict[left_term]["fuzzy_score"] = resp_match[1]
-                my_dict[left_term]["match"] = resp_match[0]
-            else:
-                my_dict[left_term]["fuzzy_score"] = 0
-                my_dict[left_term]["match_type"] = "none"
-                my_dict[left_term]["match"] = ""
-
-    df = term_alignment_dict2df(my_dict)
-    # Indicate where  match_term is duplicated
-    # ic(df.match_type.value_counts())
-    tmp_df = df.match.value_counts().rename_axis('unique_values').to_frame('counts').reset_index().query('counts > 1')
-    dup_set = set(tmp_df.unique_values.tolist())
-    # ic(dup_set)
-    df['match_term_duplicated'] = df['match'].apply(lambda x: True if x in dup_set and x != "" else False)
-    # ic(df.head(20))
-    # print(df.to_markdown(index=False))
-
-    return df
 
 
 pass
@@ -1086,8 +1018,9 @@ def main():
     mixs_v6_dict = process_mixs_dict(my_dict_v6, linkml_mixs_dict)
     mixs_v6_obj = mixs(mixs_v6_dict, "mixs_v6", linkml_mixs_dict)
 
-    #compareAllTerms(ena_cl_obj.get_all_term_list(), mixs_v6_obj.get_all_term_list())
-    # sys.exit()
+    df = compareAllTerms(ena_cl_obj.get_all_term_list(), mixs_v6_obj.get_all_term_list())
+    print(df.to_markdown(index = False))
+    sys.exit()
 
     compareSelectChecklists(ena_cl_obj, mixs_v6_obj, report)
 
--- source/pairwise_term_matches.py
+++ source/pairwise_term_matches.py
@@ -1,6 +1,7 @@
 from icecream import ic
 import pandas as pd
 from clean_terms import *
+from rapidfuzz import process
 
 class pairwise_term_matches:
     """pairwise_term_matches object as simple object orientated to reduce complexity and saves passing a big hash.
@@ -123,3 +124,76 @@ class pairwise_term_matches:
                           columns = [self.left_name, self.right_name]).sort_values(self.left_name)
 
         return df
+
+
+def term_alignment_dict2df(my_dict):
+    """
+
+    :param my_dict:
+    :return: df
+    df = df[["left_term", "match_type", "match", "fuzzy_score"]]
+    """
+    df = pd.DataFrame.from_dict(my_dict, orient = 'index')
+    df["left_term"] = df.index
+    df = df[["left_term", "match_type", "match", "fuzzy_score"]]
+    df["fuzzy_score"] = df["fuzzy_score"].astype(int)
+    return df
+
+def compareAllTerms(left_list, right_list):
+    """
+
+    :param left_list:
+    :param right_list:
+    :return: df: #['left_term', 'match_type:exact|harmonised|fuzzy|none', 'match', 'fuzzy_score', 'match_term_duplicated:boolean']
+    """
+    #make sure that the lists are unique and sorted, note the slight name change, in case we need the original list again
+    left_term_set = set(left_list)
+    right_term_set = set(right_list)
+    left_term_list = sorted(left_term_set)
+    right_term_list = sorted(right_term_set)
+    # ic(f"left len={len(left_term_list)} right len={len(right_term_list)}")
+
+    left_clean_dict, left_raw_dict = generate_clean_dict(left_term_list)
+    right_clean_dict, right_raw_dict = generate_clean_dict(right_term_list)
+
+    left_clean_set = set(left_clean_dict.keys())
+    right_clean_set = set(right_clean_dict.keys())
+    exact_intersection_set = left_term_set.intersection(right_term_set)
+    clean_intersection_set = left_clean_set.intersection(right_clean_set)
+
+    my_dict = {}
+    fuzzy_threshold = 85
+    for left_term in left_term_list:
+        my_dict[left_term] = {"match_type": "none", "fuzzy_score": 100}
+        left_clean = left_raw_dict[left_term]
+        # print(f"{left_term} - {left_clean}")
+        if left_term in exact_intersection_set:
+            my_dict[left_term]["match"] = left_term
+            my_dict[left_term]["match_type"] = "exact"
+        elif left_clean in clean_intersection_set:
+            my_dict[left_term]["match"] = right_clean_dict[left_clean]
+            my_dict[left_term]["match_type"] = "harmonised"
+        else:
+            resp_match = process.extractOne(left_term, right_term_list)
+            if resp_match[1] > fuzzy_threshold:
+                my_dict[left_term]["match_type"] = "fuzzy"
+                my_dict[left_term]["fuzzy_score"] = resp_match[1]
+                my_dict[left_term]["match"] = resp_match[0]
+            else:
+                my_dict[left_term]["fuzzy_score"] = 0
+                my_dict[left_term]["match_type"] = "none"
+                my_dict[left_term]["match"] = ""
+
+    df = term_alignment_dict2df(my_dict)
+    # Indicate where  match_term is duplicated
+    # ic(df.match_type.value_counts())
+    tmp_df = df.match.value_counts().rename_axis('unique_values').to_frame('counts').reset_index().query('counts > 1')
+    dup_set = set(tmp_df.unique_values.tolist())
+    # ic(dup_set)
+    df['match_term_duplicated'] = df['match'].apply(lambda x: True if x in dup_set and x != "" else False)
+    # ic(df.head(20))
+    # print(df.to_markdown(index=False))
+
+    return df
+
+
