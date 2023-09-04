50c34707d0b728431b5483b0bf2f7f60a372daba
woollard <woollard@ebi.ac.uk>
2023-09-04 16:48:38 +0100

    tests created for all the clean functions

---
+++ source/test_clean_terms.py
@@ -0,0 +1,31 @@
+import unittest
+from clean_terms import clean_term, clean_list, generate_clean_dict, cleanList2set
+from icecream import ic
+
+class Test(unittest.TestCase):
+
+    def test_clean_term(self):
+        test_in = 'Some-Term'
+        test_out = 'some_term'
+        self.assertEqual(clean_term(test_in), test_out)
+    def test_clean_list(self):
+        test_in = ['Some-Term', 'another_TERM']
+        test_out = ['some_term', 'another_term']
+        self.assertListEqual(clean_list(test_in), test_out)
+
+    def test_generate_clean_dict(self):
+        test_list = ['Some-Term']
+        test_clean_out = {'some_term': 'Some-Term'}
+        test_raw_out = {'Some-Term': 'some_term'}
+        clean_dict, raw_dict = generate_clean_dict(test_list)
+        self.assertDictEqual(clean_dict, test_clean_out)
+        self.assertDictEqual(raw_dict, test_raw_out)
+
+    def test_cleanList2set(self):
+        test_in = ['Some-Term', 'another_TERM', 'another_TERM']
+        test_out = set(['some_term','another_term'])
+        self.assertSetEqual(cleanList2set(test_in), test_out)
+
+
+if __name__ == '__main__':
+    unittest.main()
